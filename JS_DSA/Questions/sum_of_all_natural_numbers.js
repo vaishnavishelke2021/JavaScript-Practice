98b6da92c3b4b49a2b68ce106fcca2165b02b2b2
Vaishnavi Shelke <89862255+vaishnavishelke2021@users.noreply.github.com>
2024-06-30 16:21:49 +0000

    sum of all n natural numbers

---
+++ JS_DSA/Questions/sum_of_all_natural_numbers.js
@@ -0,0 +1,14 @@
+// Sum of all natural numbers from 1 to n 
+
+
+function sumOfAllNaturalNums(n){
+    // let sum = 0
+    // for (let i=0; i<=n; i++){
+    //     sum += i
+    // }
+    // return sum
+
+    return (n*(n+1))/2
+}
+
+console.log(sumOfAllNaturalNums(5)) 
