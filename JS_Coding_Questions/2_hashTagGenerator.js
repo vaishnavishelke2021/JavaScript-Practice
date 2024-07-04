a73f7755ed23c6acec3b2d2c1975c934009aaedf
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-04 20:28:18 +0530

    Q.2 : Hash tag generator from string

---
+++ JS_Coding_Questions/2_hashTagGenerator.js
@@ -0,0 +1,33 @@
+// ---------------------------------------------------------------
+//  Programming Question: Hash Tag Generator
+// ---------------------------------------------------------------
+
+//? You are required to implement a function generateHash that generates a hash tag from a given input string. The hash tag should be constructed as follows:
+
+//? The input string should be converted to a hash tag format, where each word is capitalized and concatenated together without spaces.
+//? If the length of the input string is greater than 280 characters or if the input string is empty or contains only whitespace, the function should return false.
+//? Otherwise, the function should return the generated hash tag prefixed with #.
+
+//* Write a function generateHash to accomplish this task.
+
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+function generateHash(inputString) {
+  if (inputString.length > 280 || inputString.trim().length === 0) return false;
+
+  let words = inputString.split(" ");
+  //   console.log(words);
+
+  //   -------------------------------------------way 1------------------------------------------------
+  //   let hashTag = words.map(
+  //     (word) => word.replace(word.at(0), word.at(0).toUpperCase()) // or charAt
+  //   );
+  //   console.log(hashTag);
+
+  //   -------------------------------------------way 2------------------------------------------------
+  let hashTag = words.map((word) => word[0].toUpperCase() + word.slice(1));
+
+  return `#${hashTag.join("")}`;
+}
+
+console.log(generateHash("Hello world"));
