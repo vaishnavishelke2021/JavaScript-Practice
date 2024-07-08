6608c15073dbaa606bbdc6f009386b43afa8e434
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-08 23:29:12 +0530

    Q.12 : Remove duplicates from array

---
+++ JS_Coding_Questions/12_removeDuplicates.js
@@ -0,0 +1,35 @@
+//? 12: Write a function that takes an array of integers as input and removes any duplicate elements, returning a new array with only the unique elements.
+
+// Example usage:
+// console.log(removeDuplicates([1, 2, 3, 2, 1, 4])); // Output: [1, 2, 3, 4]
+// console.log(removeDuplicates([5, 6, 7, 7, 8, 8, 91])); // Output: [5, 6, 7, 8,9]
+// console.log(removeDuplicates([1, 2, 3, 4])); // Output: [1, 2, 3, 4)
+// console.log(removeDuplicates([])); // Output: []
+
+// The new Set() method in JavaScript creates a new Set object. A Set object is a collection of unique values. It can store any type of value, whether primitive values or object references.
+
+// Constraints:
+// The input array may contain both positive and negative integers.
+// The input array may be empty-l
+// The elements in the input array are not guaranteed to be sorted.
+// The output array should retain the original order of elements from the input array.
+
+function removeDuplicates(arr) {
+  // -------------------------------way 1------------------------------
+  // let newarr = [];
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (!newarr.includes(arr[i])) {
+  //     newarr.push(arr[i]);
+  //   }
+  // }
+  // return newarr;
+
+  // -------------------------------way 1------------------------------
+  let unique = [...new Set(arr)];
+  //   console.log(unique);
+  return unique;
+}
+
+console.log(removeDuplicates([1, 2, 3, 2, 1, 4]));
+console.log(removeDuplicates([5, 6, 7, 7, 8, 8, 9, 10]));
+console.log(removeDuplicates([]));
