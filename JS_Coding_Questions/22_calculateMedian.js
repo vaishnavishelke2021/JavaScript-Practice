6f6d13f01866a0543a8e4582545d09ce78f2c201
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-19 21:21:15 +0530

    Q.22 : Calculate median

---
+++ JS_Coding_Questions/22_calculateMedian.js
@@ -0,0 +1,30 @@
+//? Write a JavaScript function findMedian(arr) that takes an array of numbers as input and returns the median value. If the array has an even number of elements, return the average of the two middle values.
+
+//? For example, the median of 3, 3, 5, 9, 15 is 5. If there is an even number of observations, then there is no single middle value; the median is then usually defined to be the mean of the two middle values: so the median of 3, 5, 7, 9 is (5+7)/2 = 6.
+
+//todo Tips
+//? Sort the array in ascending order.
+//? If the array has an odd number of elements, the median is the middle element.
+//? If the array has an even number of elements, the median is the average of the two middle elements.
+
+//* Steps:
+// 1. Sort the array in ascending order.
+// 2. Check if length is even/odd
+// 3. condition: if even
+// 4. condition: if odd
+
+function findMedian(arr) {
+  arr.sort((a, b) => a - b);
+
+  let mid = Math.floor(arr.length / 2);
+  if (arr.length % 2 === 0) {
+    // console.log(arr[mid]);
+    return (arr[mid] + arr[mid - 1]) / 2;
+  } else {
+    return arr[mid];
+  }
+}
+
+console.log(findMedian([5, 3, 9, 1, 71])); // Output: 5
+console.log(findMedian([2, 4, 6, 8])); // Output: 5
+console.log(findMedian([1, 3, 5, 7, 9, 11])); // Output: 6
