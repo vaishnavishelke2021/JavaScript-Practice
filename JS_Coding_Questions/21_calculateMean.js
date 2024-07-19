62d30658a905c7063176c13cb488901f92e167c4
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-19 20:34:26 +0530

    Q.21 : Calculate mean

---
+++ JS_Coding_Questions/21_calculateMean.js
@@ -0,0 +1,22 @@
+//? Write a function called calculateMean that takes an array of numbers as input and returns the mean (average) of those numbers.
+//todo In math, the mean is the average of a set of numbers, or the numeric value that represents the center of a collection of numbers.
+
+//? Constraints:
+//? The input array may contain positive and negative integers.
+//? The input array may be empty. If it is empty, the function should return 0.
+// console.log(calculateMean([1, 2, 3, 4, 5])); // Output: 3
+// console.log(calculateMean([10, 20, 30])); // Output: 20
+// console.log(calculateMean([-1, 0, 1])); // Output: 0
+// console.log(calculateMean([])); // Output: 0
+
+function calculateMean(arr) {
+  if (arr.length === 0) return 0;
+  let sum = arr.reduce((acc, curr) => acc + curr, 0);
+  //   console.log(sum);
+  return sum / arr.length;
+}
+
+console.log(calculateMean([1, 2, 3, 4, 5]));
+console.log(calculateMean([10, 20, 30]));
+console.log(calculateMean([-1, 0, 1]));
+console.log(calculateMean([]));
