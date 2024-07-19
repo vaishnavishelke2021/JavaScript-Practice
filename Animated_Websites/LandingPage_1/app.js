4f288cb622147bf9791db6fddba86e48d8293493
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-19 17:13:35 +0530

    Header added

--- Animated_Websites/LandingPage_1/app.js
+++ Animated_Websites/LandingPage_1/app.js
@@ -1,16 +1,28 @@
-// const tl = tim;
+const tl = gsap.timeline();
 
-gsap.from("nav h2", {
+tl.from("nav h2", {
   y: -30,
   opacity: 0,
   duration: 1,
-  delay: 0.1,
 });
 
-gsap.from("nav ul li", {
+tl.from("nav ul li", {
   y: -30,
   opacity: 0,
   duration: 1,
-  delay: 0.4,
-  stagger: 0.15,
+  stagger: 0.1,
+});
+
+tl.from("#header h1", {
+  x: -100,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.2,
+});
+
+tl.from("#header img", {
+  x: 200,
+  opacity: 0,
+  rotate: 40,
+  stagger: 0.3,
 });
