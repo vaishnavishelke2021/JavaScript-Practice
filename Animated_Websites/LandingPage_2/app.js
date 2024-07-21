3e74c3852cdf36fb8a51f8770970871c2cd30194
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-21 19:04:03 +0530

    header/container animation added

--- Animated_Websites/LandingPage_2/app.js
+++ Animated_Websites/LandingPage_2/app.js
@@ -9,9 +9,42 @@ tl.from("nav h1", {
 });
 
 tl.from("nav ul li,nav ul button", {
-    y: -20,
+  y: -20,
+  opacity: 0,
+  duration: 0.6,
+  ease: "power1.out",
+  stagger: 0.1,
+});
+
+tl.from("#container .containerL h1", {
+  x: -200,
+  opacity: 0,
+  duration: 0.7,
+  ease: "power1.out",
+}, "-=0.3");
+
+tl.from("#container .containerL p", {
+  x: -150,
+  opacity: 0,
+  duration: 0.5,
+  ease: "power1.out",
+});
+
+tl.from("#container .containerL button", {
+  x: -150,
+  opacity: 0,
+  duration: 0.5,
+  ease: "power1.out",
+});
+
+tl.from(
+  "#container .containerR img",
+  {
     opacity: 0,
-    duration: 0.6,
+    scale: 0,
+    duration: 1,
+    delay: 0.3,
     ease: "power1.out",
-    stagger: 0.1
-  });
+  },
+  "-=0.8"
+);
