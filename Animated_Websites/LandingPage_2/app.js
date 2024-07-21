97713d7b53fda88d7b4656deb2a24f11996d7e89
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-21 22:12:35 +0530

    page3 added

--- Animated_Websites/LandingPage_2/app.js
+++ Animated_Websites/LandingPage_2/app.js
@@ -141,7 +141,7 @@ tl2.from("#servicesContainer .s3", {
     scroller: "body",
     // markers: true,
     start: "top 70%",
-    end: "top 100%",
+    end: "top 40%",
     scrub: 1,
   },
 });
@@ -156,7 +156,87 @@ tl2.from("#servicesContainer .s4", {
     scroller: "body",
     // markers: true,
     start: "top 70%",
-    end: "top 100%",
+    end: "top 40%",
+    scrub: 1,
+  },
+});
+
+var tl3 = gsap.timeline();
+
+tl3.from("#page3 #page3Container", {
+  y: 100,
+  opacity: 0,
+  duration: 1.5,
+  delay: 0.5,
+  ease: "power1.out",
+  scrollTrigger: {
+    trigger: "#page3 #page3Container",
+    scroller: "body",
+    // markers: true,
+    start: "top 60%",
+    end: "top 30%",
+    scrub: 1,
+  },
+});
+
+tl3.from("#caseStudy h1", {
+  x: -100,
+  opacity: 0,
+  duration: 1,
+  ease: "power1.out",
+  scrollTrigger: {
+    trigger: "#caseStudy h1",
+    scroller: "body",
+    // markers: true,
+    start: "top 80%",
+    end: "top 40%",
+    scrub: 1,
+  },
+});
+
+tl3.from("#caseStudy p", {
+  x: -100,
+  opacity: 0,
+  duration: 1,
+  ease: "power1.out",
+  scrollTrigger: {
+    trigger: "#caseStudy h1",
+    scroller: "body",
+    // markers: true,
+    start: "top 80%",
+    end: "top 40%",
+    scrub: 1,
+  },
+});
+
+tl3.from("#caseStudyLinks", {
+  y: 100,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+  ease: "power1.out",
+  scrollTrigger: {
+    trigger: "#caseStudyLinks",
+    scroller: "body",
+    // markers: true,
+    start: "top 90%",
+    end: "top 60%",
+    scrub: 1,
+  },
+});
+
+tl3.from("#caseStudyLinks .linkdiv", {
+  x: -20,
+  opacity: 0,
+  duration: 1,
+  ease: "power1.out",
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: "#caseStudyLinks .linkdiv",
+    scroller: "body",
+    markers: true,
+    start: "top 80%",
+    end: "top 60%",
     scrub: 1,
   },
 });
