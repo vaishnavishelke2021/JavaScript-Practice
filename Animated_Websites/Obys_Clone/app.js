98e0d94c5bfc16a006ee42e38995689691741452
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-07-25 20:53:44 +0530

    loader created

---
+++ Animated_Websites/Obys_Clone/app.js
@@ -0,0 +1,54 @@
+var tl = gsap.timeline();
+
+tl.from(".line h1", {
+  y: 100,
+  duration: 0.6,
+  delay: 0.4,
+  stagger: 0.15,
+});
+
+tl.from(
+  ".counting",
+  {
+    scale: 0,
+    duration: 0.3,
+  },
+  "-=0.7"
+);
+
+let count = document.querySelector(".count");
+let grow = 0;
+setInterval(() => {
+  if (grow < 100) {
+    count.innerHTML = grow++;
+  } else {
+    count.innerHTML = 100;
+  }
+}, 30);
+
+tl.from(
+  "#line3 h2",
+  {
+    scale: 0,
+    opacity: 0,
+    duration: 0.2,
+  },
+  "-=0.2"
+);
+
+tl.to("#loader", {
+  opacity: 0,
+  duration: 1,
+  delay: 2.2,
+});
+
+tl.from("#page1", {
+  opacity: 0,
+  duration: 0.5,
+  delay: 0.2,
+  y: 1000,
+});
+
+tl.to("#loader", {
+  display: "none",
+});
